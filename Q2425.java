4f51db1c6895758059210ea4dcb1cfe1a71b7ca1
Naveenkumar7125 <naveenkumart906@gmail.com>
2025-01-16 10:52:45 +0530

    Refered @DhamodharanSP 's Approach

--- Q2425.java
+++ Q2425.java
@@ -1,16 +1,16 @@
 class Solution {
     public int xorAllNums(int[] nums1, int[] nums2) {
-        int res = 0;
+        int r = 0;
         if(nums2.length%2==1)
         {
             for(int num : nums1)
-                res ^= num;
+                r ^= num;
         }
         if(nums1.length%2==1)
         {
             for(int num : nums2)
-                res ^= num;
+                r ^= num;
         }
-        return res;
+        return r;
     }
 }
