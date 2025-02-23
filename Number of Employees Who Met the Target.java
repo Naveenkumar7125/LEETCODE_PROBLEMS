892b8409d903d61afc7e3de95c7be69590b4b175
NAVEENKUMAR T <22cs054@nandhaengg.org>
2025-02-23 23:34:07 +0530

    Update Number of Employees Who Met the Target.java

--- Number of Employees Who Met the Target.java
+++ Number of Employees Who Met the Target.java
@@ -1,10 +1,10 @@
 class Solution {
     public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
-        int c = 0;
+        int res = 0;
         for(int i=0;i<hours.length;i++)
         {
-            c += (hours[i] >= target) ? 1 : 0;
+            res += (hours[i] >= target) ? 1 : 0;
         }
-        return c;
+        return res;
     }
 }
