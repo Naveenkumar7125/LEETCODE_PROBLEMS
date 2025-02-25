5308e9e9a6fc516982913bbec765f037783aa398
NAVEENKUMAR T <22cs054@nandhaengg.org>
2025-02-25 23:13:21 +0530

    1 minute problem

---
+++ 2520.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countDigits(int num) {
+        int count = 0;
+        int n = num;
+        while(num!=0)
+        {
+            if(n%(num%10)==0)count++;
+            num /= 10;
+        }
+        return count;
+    }
+}
