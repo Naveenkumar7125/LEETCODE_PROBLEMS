808f23be3abe343a4c571f5b7159696105bd6cb5
Naveenkumar7125 <22cs054@nandhaengg.org>
2025-03-29 14:55:06 +0530

    Added yestarday's problem

---
+++ 1207.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
+        for(int i=0;i<arr.length;i++)
+        {
+            map.put(arr[i], map.getOrDefault(arr[i],0) + 1);
+        }
+       for (int freq : map.values()) 
+       {
+            if (!set.add(freq)) 
+            { 
+                return false;
+            }
+        }
+        return true;
+    }
+}
